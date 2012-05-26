d452ebb200b4776ddbda6952f1b05e89ebec77bd
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-05-26 16:06:11 +0100

    Sanitise import

--- checklisthq/main/models.py
+++ checklisthq/main/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib import auth
+from django.contrib.auth.models import User
 
 class Checklist(models.Model):
     title = models.CharField(max_length=512)
-    owner = models.ForeignKey(auth.models.User)
+    owner = models.ForeignKey(User)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True,auto_now_add=True)
