3bc59fff16ae9d8b69c313e3d61f026d89330d55
Nicholas H.Tollervey <ntoll@ntoll.org>
2012-05-27 00:24:17 +0100

    Semi-working user checklists.

--- checklisthq/main/forms.py
+++ checklisthq/main/forms.py
@@ -1,136 +1,28 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import Textarea
 
-INITIAL_TEXT = """= WHO SURGICAL CHECKLIST: 1ST EDITION =
+from models import Checklist
 
----
-// Checklist DSL version - an example checklist with annotations.
-// Created by Wai Keong Wong, updated by Nicholas H.Tollervey
-// Date: 25/04/2012
-// Version 1.1
-
-// This is a comment because it starts with two slash characters. It won't be rendered on the web-form.
-
-// Placing text between equals characters makes it a heading. Use more equals characters to make the heading smaller.
-
-== SIGN IN ==
-
-// Text on its own becomes a paragraph:
-
-PATIENT HAS CONFIRMED
-
-// Items in the checklist start with empty square brackets making them look like a checkbox.
-
-[] IDENTITY
-[] SITE
-[] PROCEDURE
-[] CONSENT
-
-// Different sections of the checklist are delineated with three or more minus signs rendered as a horizontal line:
-
----
-
-[] SITE MARKED/NOT APPLICABLE
-
----
-
-[] ANAESTHESIA SAFETY CHECK COMPLETED
-
----
-
-[] PULSE OXIMETER ON PATIENT AND FUNCTIONING
-
----
-
-=== DOES PATIENT HAVE ===
-
-// Items that start with empty parenthesis belong to a checklist group.
-// Only one item in the checklist group can be selected.
-
-A KNOWN ALLERGY?
-() NO
-() YES
-
----
-
-DIFFICULT AIRWAY/ASPIRATION RISK?
-() NO
-() YES, AND EQUIPMENT/ASSISTANCE AVAILABLE
-
----
-
-RISK OF >500ML BLOOD LOSS (7ML/KG IN CHILDREN)?
-() NO
-() YES, AND ADEQUATE INTRAVENOUS ACCESS AND FLUIDS PLANNED
-
----
-
-== TIME OUT ==
-
-[] CONFIRM ALL TEAM MEMBERS HAVE INTRODUCED THEMSELVES BY NAME AND ROLE
-
----
-
-SURGEON, ANAESTHESIA PROFESSIONAL AND NURSE VERBALLY CONFIRM
-
-// Roles associated with specific items are listed at the start of the item within curly brackets like this:
-
-[] {SURGEON, ANAESTHESIST, NURSE} PATIENT
-[] {SURGEON, ANAESTHESIST, NURSE} SITE
-[] {SURGEON, ANAESTHESIST, NURSE} PROCEDURE
-
----
-
-ANTICIPATED CRITICAL EVENTS
-
-// Use [...] to make an item into a text box for users to add comments and other textual information.
-
-[...] {SURGEON} WHAT ARE THE CRITICAL OR UNEXPECTED STEPS, OPERATIVE DURATION, ANTICIPATED BLOOD LOSS?
-[...] {ANAESTHESIST} ARE THERE ANY PATIENT-SPECIFIC CONCERNS?
-[...] {NURSE} HAS STERILITY (INCLUDING INDICATOR RESULTS) BEEN CONFIRMED?
-[...] {NURSE} ARE THERE EQUIPMENT ISSUES OR ANY CONCERNS?
-
----
-
-HAS ANTIBIOTIC PROPHYLAXIS BEEN GIVEN WITHIN THE LAST 60 MINUTES?
-() YES
-() NOT APPLICABLE
-
----
-
-IS ESSENTIAL IMAGING DISPLAYED?
-() YES
-() NOT APPLICABLE
-
----
-
-== SIGN OUT ==
-
-NURSE VERBALLY CONFIRMS WITH THE TEAM
-[] {NURSE} THE NAME OF THE PROCEDURE RECORDED
-[] {NURSE} THAT INSTRUMENT, SPONGE AND NEEDLE COUNTS ARE CORRECT (OR NOT APPLICABLE)
-[] {NURSE} HOW THE SPECIMEN IS LABELLED (INCLUDING PATIENT NAME)
-[] {NURSE} WHETHER THERE ARE ANY EQUIPMENT PROBLEMS TO BE ADDRESSED
-
----
-
-[...] {SURGEON, ANAESTHESIST, NURSE} REVIEW THE KEY CONCERNS FOR RECOVERY AND MANAGEMENT OF THIS PATIENT
-
-// Click the "Submit" button below to see this checklist rendered as a web form.
-// Experiment and play! All feedback most welcome."""
-
-class DSLForm(forms.Form):
+class ChecklistForm(forms.ModelForm):
     """
     A form for entering the specification of a checklist.
-    """
+    title = forms.CharField(
     specification = forms.CharField(
         label='',
-        initial=INITIAL_TEXT,
+        initial='',
         widget=forms.Textarea(attrs={
         'class': 'input-xlarge',
         'style': 'width: 100%',
         'rows': 20})
-    )
+    )"""
+    class Meta:
+        model = Checklist
+        fields = ('title', 'content')
+        widgets = {
+            'content': Textarea(attrs={'rows': 16, 'style': 'width:100%;',
+                'class': 'input-xlarge'}),
+        }
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(
--- checklisthq/main/views.py
+++ checklisthq/main/views.py
@@ -7,7 +7,8 @@ from django.contrib.auth.models import User
 
 from checklistdsl import lex, parse
 
-from forms import DSLForm, NewUserForm
+from forms import ChecklistForm, NewUserForm
+from models import Checklist
 
 _logger = logging.getLogger(__name__)
 
@@ -15,21 +16,7 @@ def home(request):
     """
     Simplest possible case.
     """
-    form = DSLForm
-    result = ''
-    if request.method == 'POST':
-        form = DSLForm(request.POST)
-        if form.is_valid():
-            raw = form.cleaned_data['specification']
-            tokens = lex.get_tokens(raw)
-            result = parse.get_form(tokens)
-
-    context = {
-        'form': form,
-        'result': result
-    }
-
-    return render(request, 'home.html', context)
+    return render(request, 'frontpage.html')
 
 def new_user(request):
     if request.method == 'POST':
@@ -44,3 +31,65 @@ def new_user(request):
     context = {}
     context['form'] = form
     return render(request, 'registration/new_user.html', context)
+
+def checklist_list(request, username):
+    context = {}
+    try:
+        owner = User.objects.get(username=username)
+    except User.DoesNotExist:
+        # TODO: Return a 404 when we've configured it.
+        return HttpResponseRedirect('/')
+    checklists = Checklist.objects.filter(owner=owner).order_by('-modified')
+    is_own_checklists = owner == request.user
+    context['checklists'] = checklists
+    context['is_own_checklists'] = is_own_checklists
+    context['owner'] = owner
+    return render(request, 'user/checklist_list.html', context)
+
+def new_checklist(request):
+    """
+    Creates a new checklist.
+    """
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+    form = ChecklistForm()
+    context = {}
+    if request.method == 'POST':
+        form = ChecklistForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            user = request.user
+            checklist = Checklist.objects.create(
+                title=title,
+                content=content,
+                owner=user
+            )
+            context['saved'] = True
+    context['form'] = form
+    return render(request, 'user/edit_checklist.html', context)
+
+def view_checklist(request, id):
+    checklist = Checklist.objects.get(id=id)
+    tokens = lex.get_tokens(checklist.content)
+    result = parse.get_form(tokens)
+    context = {
+        'checklist': checklist,
+        'result': result
+    }
+    return render(request, 'view_checklist.html', context)
+
+def edit_checklist(request, id):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+    checklist = Checklist.objects.get(id=id)
+    context = {}
+    if request.method == 'POST':
+        form = ChecklistForm(request.POST, instance=checklist)
+        if form.is_valid():
+            form.save()
+            context['saved'] = True
+    else:
+        form = ChecklistForm(instance=checklist)
+    context['form'] = form
+    return render(request, 'user/edit_checklist.html', context)
