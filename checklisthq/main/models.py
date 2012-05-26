4de1ebde779f999bb1ecdf5529905a86312c0d53
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-05-26 15:10:07 +0100

    Initial Checklist model

--- checklisthq/main/models.py
+++ checklisthq/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib import auth
 
-# Create your models here.
+class Checklist(models.Model):
+    title = models.CharField(max_length=512)
+    owner = models.ForeignKey(auth.models.User)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True,auto_now_add=True)
+    # To add: tags, type
