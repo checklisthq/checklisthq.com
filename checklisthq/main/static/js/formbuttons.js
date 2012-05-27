cde99fe4343dc1e022cc0fe8f14ed4799cb37fb1
Alex Mathy <mathy@Alex-Mathys-MacBook-Pro.local>
2012-05-27 11:26:47 +0100

    checklist editor

--- checklisthq/main/static/js/formbuttons.js
+++ checklisthq/main/static/js/formbuttons.js
@@ -1,42 +1,65 @@
+var loadedmodal=false;
+var global_pre="";
+var global_post="";
 $("#closebutton").click(function(e) {
   $("#myModal").hide()
 });
 
 function modal(type, text, pre, post) {
-  $("#modaltitle").val(text);
+
+  $("#modaltitle").html(text);
   $("#myModal").show();
   $("#myModal input").focus();
-  $("#savebutton").click(function(e) {
+
+ global_pre=pre;
+ global_post=post;
+
+  if (!loadedmodal){
+	loadedmodal=true;
+  	$("#savebutton").click(submitmodal);
+    $("#myModal").keypress(function(e) {
+    if(e.keyCode == 13) {
+        submitmodal(e);
+    }
+	});
+
+   }
+}
+
+function submitmodal(e){
     $("#myModal").hide()
-    $("#id_specification").val($("#id_specification").val()+pre+$("#modalval").val()+post+"\n");
+    $("#id_content").val($("#id_content").val()+global_pre+$("#modalval").val()+global_post+"\n");
     $("#modalval").val("");
     return false;
-  });
 }
 
 $("#backbutton").click(function(e) {
   history.go(-1);
 });
-$("#id_specification").focus();
+$("#id_content").focus();
 $("#clear").click(function(e) {
-  $("#id_specification").val("");
-  $("#id_specification").focus();
+  $("#id_content").val("");
+  $("#id_content").focus();
 });
 
 $("#addheading").click(function(e) {
   modal("heading", "Add Heading", "=", "=");
+  return false;
 });
 
 $("#addchecklistitem").click(function(e) {
   modal("checklistitem", "Add Checklist Item", "[]", "");
+  return false;
 });
 
 $("#addchecklistgroupitem").click(function(e) {
   modal("checklistgroupitem", "Add Checklist group item", "()", "");
+  return false;
 });
 
 $("#addtextbox").click(function(e) {
-  modal("textbox", "Add textbox", "[...]", "");
+  modal("Textbox", "Add textbox", "[...]", "");
+  return false;
 });
 
 $("#preview").click(function(e) {
